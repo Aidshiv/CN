61a332104f23ce3a9e30dabb77a11d8228f91c4a
Aidshiv <163630330+Aidshiv@users.noreply.github.com>
2024-12-18 23:40:01 +0530

    Bellman-Ford algorithm.java

--- Bellman-Ford algorithm.java
+++ Bellman-Ford algorithm.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
 
 public class BellmanFord {
-    private int D[];
+    private int[] D;
     private int NoV;
     public static final int MAX_VALUE = 999;
 
+    // Constructor to initialize BellmanFord with number of vertices
     public BellmanFord(int NoV) {
         this.NoV = NoV;
         D = new int[NoV + 1];
     }
 
+    // Method to perform Bellman-Ford algorithm to find shortest paths
     public void BellmanFordEvaluation(int source, int A[][]) {
+        // Initialize the distance array with maximum values
         for (int node = 1; node <= NoV; node++) {
             D[node] = MAX_VALUE;
         }
         D[source] = 0;
 
+        // Relax edges (NoV - 1) times
         for (int node = 1; node <= NoV - 1; node++) {
             for (int i = 1; i <= NoV; i++) {
                 for (int j = 1; j <= NoV; j++) {
@@ -28,6 +32,7 @@ public class BellmanFord {
             }
         }
 
+        // Check for negative weight cycles
         for (int i = 1; i <= NoV; i++) {
             for (int j = 1; j <= NoV; j++) {
                 if (A[i][j] != MAX_VALUE) {
@@ -39,21 +44,23 @@ public class BellmanFord {
             }
         }
 
+        // Print the shortest distances from the source
         for (int vertex = 1; vertex <= NoV; vertex++) {
             System.out.println("The shortest distance from source " + source + " to vertex " + vertex + " is: " + D[vertex]);
         }
     }
 
-    public static void main(String... arg) {
+    public static void main(String[] arg) {
         int NoV = 0;
         int source;
         Scanner scanner = new Scanner(System.in);
 
+        // Input the number of vertices
         System.out.println("Enter the number of vertices:");
         NoV = scanner.nextInt();
 
-        int A[][] = new int[NoV + 1][NoV + 1];
-
+        // Input the adjacency matrix
+        int[][] A = new int[NoV + 1][NoV + 1];
         System.out.println("Enter the adjacency matrix:");
         for (int i = 1; i <= NoV; i++) {
             for (int j = 1; j <= NoV; j++) {
@@ -61,15 +68,19 @@ public class BellmanFord {
             }
         }
 
+        // Input the source vertex
         System.out.println("Enter the source vertex:");
         source = scanner.nextInt();
 
+        // Create BellmanFord object and call the BellmanFordEvaluation method
         BellmanFord bellmanford = new BellmanFord(NoV);
         bellmanford.BellmanFordEvaluation(source, A);
 
+        // Close the scanner
         scanner.close();
     }
 }
+
 
 // *********input**********
 // Enter the number of vertices:
